6f3ab9ee8bcc0b0fb28b8999f69cb6b4772eb10c
Matt Hoover <matt@hoover.ml>
2017-09-20 11:04:47 +0000

    Refactor tests - Add `full_title()` check for remaining pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,15 +6,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     ## Static Pages
     # Test /
     get root_path
+    assert_select "title", full_title
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", help_path
 
+    # Test /about
+    get about_path
+    assert_select "title", full_title("About")
+
     # Test /contact
     get contact_path
     assert_select "title", full_title("Contact")
+
+    # Test /help
+    get help_path
+    assert_select "title", full_title("Help")
 
     ## Users Pages
     # Test /signup
