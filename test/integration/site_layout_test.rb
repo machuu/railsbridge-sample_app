629a8c26eb6f308acdab78dcffdc6a381d86ae76
Matt Hoover <matt@hoover.ml>
2017-09-20 10:51:40 +0000

    Refactor tests
    - Organize without adding anything new
    - Change users `new` test to refer to `signup`
    - Separate **layout** tests by controller / page name

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,16 +3,23 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
   test 'layout links' do
+    ## Static Pages
+    # Test /
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    
+    assert_select "a[href=?]", help_path
+
+    # Test /contact
     get contact_path
     assert_select "title", full_title("Contact")
+
+    ## Users Pages
+    # Test /signup
     get signup_path
     assert_select "title", full_title("Sign up")
+
   end
 end
