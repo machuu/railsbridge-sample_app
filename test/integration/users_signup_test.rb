7f767b183b47aebf2352acc28f05ce87d366c2cc
Matt Hoover <matt@hoover.ml>
2017-09-26 09:10:59 +0000

    7.3.4 Exercise 3: Update "invalid user" test to use `signup_path`
    Chage `users_path` -> `signup_path` in `post` command

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,10 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup does not create new user" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
-                                        email: "user@invalid",
-                                        password:              "foo",
-                                        password_confirmation: "bar" } }
+      post signup_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
