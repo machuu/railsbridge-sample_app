615fd551b00c4248a294efefbd8357b262979825
Matt Hoover <matt@hoover.ml>
2017-09-27 11:23:25 +0000

    8.1.1 Listing 8.2: Add RESTful actions for Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   # Static Pages Routes
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
@@ -11,4 +9,9 @@ Rails.application.routes.draw do
   resources :users
   get   '/signup', to: 'users#new'
   post  '/signup', to: 'users#create'
+
+  # Sessions Routes
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
 end
