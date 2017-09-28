0efeb1da2ff80f72c28841f7ecbc2d06fa515b36
Matt Hoover <matt@hoover.ml>
2017-09-28 09:32:31 +0000

    8.1.3 Listing 8.7: Authenticate Login Attempt
    If the `user` exists, and the password matches, it's a success, but
    we don't know what to do yet.
    Otherwise, render 'new'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Successful login, redirect to user's show page
+    else
+      render 'new'
+    end
   end
 end
