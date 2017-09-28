1471185916875eb7cab9d560838f5662103a7c66
Matt Hoover <matt@hoover.ml>
2017-09-28 10:21:37 +0000

    8.2.2 Listing 8.16: Add `current_user()` to `SessionsHelper`
    This creates an effecient method of finding the currently logged in user, if
    there is one.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,9 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Returns logged in user, or 'nil'
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
