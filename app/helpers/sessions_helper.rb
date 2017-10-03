320a80a58dddecc1582ca1013eb11900c5b4f040
Matt Hoover <matt@hoover.ml>
2017-10-03 10:02:41 +0000

    9.1.2 Listing 9.7-8: Add and Call Sessions Helper `remember(user)`
    After successful login, store persistent login cookie and save token
    to user db field

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Generate new login token, store in cookie and database `digest`
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Returns logged in user, or 'nil'
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
