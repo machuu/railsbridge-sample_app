528467712c96d29b48f02cbe3b04b6d8c3d4523c
Matt Hoover <matt@hoover.ml>
2017-10-03 09:25:31 +0000

    9.1.1: Add `remember_digest` to Users DB
    Run the commands
    
    ```
    $> rails generate migration add_remember_digest_to_users remember_digest:string
    Running via Spring preloader in process 1637
          invoke  active_record
          create    db/migrate/20171003092453_add_remember_digest_to_users.rb
    $> rails db:migrate
    == 20171003092453 AddRememberDigestToUsers: migrating =========================
    -- add_column(:users, :remember_digest, :string)
       -> 0.0040s
    == 20171003092453 AddRememberDigestToUsers: migrated (0.0041s) ================
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922113713) do
+ActiveRecord::Schema.define(version: 20171003092453) do
+
+  create_table "letsencrypt_plugin_challenges", force: :cascade do |t|
+    t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "letsencrypt_plugin_settings", force: :cascade do |t|
+    t.text "private_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170922113713) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
