32a8391ce9549c1e6c1c2640956cc0ea68dbaa8b
Matt Hoover <matt@hoover.ml>
2017-10-04 09:24:44 +0000

    9.3.1 Exercise 1: Update `remembering` test to compare value of token
    Using Listings 9.27-8 as a guide
    
    Convert `user` -> `@user` in `SessionsController:create`, so *user* is now
    and instance variable, which can be accessed from integration tests.
    
    Then, update the tests:
    
    - **with remembering**: instead of *only* asserting that the current cookie
        is not empty, assert that the current cookie and the current *user* token
        are equal.
    - **without remembering**: instead of *only* asserting that the current cookie
        is empty, assert that the current *user* token is nil. (*BONUS*)

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -40,7 +40,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login with remembering' do
     log_in_as(@user, remember_me: '1')
-    assert_not_empty cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
   test 'login without remembering' do
@@ -49,5 +49,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # Log in again to verify that the cookie is deleted
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
+    assert assigns(:user).remember_token.nil?
   end
 end
