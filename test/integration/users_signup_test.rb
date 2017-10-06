1652b225e45ab4021c257d1836f5b3806bb645ba
Matt Hoover <matt@hoover.ml>
2017-10-06 09:59:59 +0000

    10.1.4 Listing 10.13: update `validates: password` in `user` model to fix failing test
    Add `allow_nil: true` to `validates: password` to allow `nil` passwords.
    This allows an `update` to pass validation when no password is entered.
    However, a new `signup` still catches a blank password.
    
    `has_secure_password` includes a separate validation to catch `nil` passwords, but
    only when the *user* does not already have a secure password stored.
    
    Also add new test to explicitly confirm new user `signup` fails with blank password

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,13 +1,26 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup" do
+  test "invalid signup - all fields invalid" do
     get signup_path
     assert_no_difference 'User.count' do
       post signup_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'form[action="/users"]'
+  end
+
+  test "invalid signup - blank password" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password:              "",
+                                         password_confirmation: "" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
