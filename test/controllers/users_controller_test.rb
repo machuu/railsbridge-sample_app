dbbf94668851f3695dc71954bdcdeed98eddfbe7
Matt Hoover <matt@hoover.ml>
2017-10-07 09:24:18 +0000

    10.2.1 Listing 10.20: Add test that `edit` and `update` are *protected*
    Verified by commenting out `before_filter` and seeing tests fail

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:testing)
+  end
+
   test "should get signup" do
     get signup_path
     assert_response :success
   end
 
+  test "should redirect edit page to login when not logged in" do
+    get edit_user_path(@user)
+    assert flash.present?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update page to login when not logged in" do
+    patch user_path(@user), params: {
+                              user: {
+                                name: @user.name,
+                                email: @user.email }  }
+    assert flash.present?
+    assert_redirected_to login_url
+  end
 end
