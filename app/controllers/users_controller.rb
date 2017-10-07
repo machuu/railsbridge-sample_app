e0ec4749c43526323a775f37a830fabab0ae4c81
Matt Hoover <matt@hoover.ml>
2017-10-07 09:09:22 +0000

    10.2.1 Listing 10.15: Restrict `edit` and `update` pages to logged_in users
    This still lets any logged in user access *any* user's `edit` and `update` pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -42,5 +43,12 @@ class UsersController < ApplicationController
                                    :email,
                                    :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in to view this page"
+        redirect_to login_url
+      end
     end
 end
