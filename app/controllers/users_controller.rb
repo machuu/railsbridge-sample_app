739cd4dc9eef07463d57916fce9eba3b399e7498
Matt Hoover <matt@hoover.ml>
2017-10-07 10:48:59 +0000

    10.2.3 Listing 10.31: `store_location` before redirecting user to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in to view this page"
         redirect_to login_url
       end
