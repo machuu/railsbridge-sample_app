7ac9eaff89548903c913884d4323da9cca15d6d0
Matt Hoover <matt@hoover.ml>
2017-10-07 09:53:51 +0000

    10.2.2 Listing 10.24: Add tests for `edit` and `update` as another user
    These fail, becuase any logged in user can access any other user's pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:test_user_1)
+    @another_user = users(:test_user_2)
   end
 
   test "should get signup" do
@@ -23,5 +24,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                 email: @user.email }  }
     assert flash.present?
     assert_redirected_to login_url
+  end
+
+  test "should redirect edit page when logged in another user" do
+    log_in_as(@another_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update page when logged in another user" do
+    log_in_as(@another_user)
+    patch user_path(@user), params: {
+                              user: {
+                                name: @user.name,
+                                email: @user.email }  }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
