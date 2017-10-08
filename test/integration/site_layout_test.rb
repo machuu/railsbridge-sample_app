90599a123fb931b35e4a47e0e564bc32eb72741c
Matt Hoover <matt@hoover.ml>
2017-10-08 10:34:52 +0000

    10.3.1 Exercise 1: Update Layout test for login changes
    Header behaves differently for Logged In users, and new links are filled in.
    
    - Check links and titles for new pages
        - Login
        - User Profile
        - Edit User
    - Test *logged in* link behavior
        - visible when logged in
        - not visible when not logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,27 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  
-  test 'layout links' do
+
+  def setup
+    @user = users(:test_user_1)
+  end
+
+  test 'layout links for non-logged in user' do
     ## Static Pages
     # Test /
     get root_path
     assert_select "title", full_title
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]",    root_path, count: 2
+    assert_select "a[href=?]",    about_path
+    assert_select "a[href=?]",    contact_path
+    assert_select "a[href=?]",    help_path
+    assert_select "a[href=?]",    login_path
+    assert_select "ul[class=?]", "dropdown-menu",         count: 0
+    assert_select "a[href=?]",    user_path(@user),       count: 0
+    assert_select "a[href=?]",    edit_user_path(@user),  count: 0
+    assert_select "a[href=?]",    users_path,             count: 0
+    assert_select "a[href=?]",    logout_path,            count: 0
 
     # Test /about
     get about_path
@@ -30,5 +40,53 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
 
+    # Test /login
+    get login_path
+    assert_select "title", full_title("Log in")
+  end
+
+  test 'layout links for logged in user' do
+    log_in_as(@user)
+
+    ## Static Pages
+    # Test /
+    get root_path
+    assert_select "title", full_title
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]",    root_path, count: 2
+    assert_select "a[href=?]",    about_path
+    assert_select "a[href=?]",    contact_path
+    assert_select "a[href=?]",    help_path
+    assert_select "a[href=?]",    login_path, count: 0
+    assert_select "ul[class=?]",  "dropdown-menu"
+    assert_select "a[href=?]",    user_path(@user)
+    assert_select "a[href=?]",    edit_user_path(@user)
+    assert_select "a[href=?]",    users_path
+    assert_select "a[href=?]",    logout_path
+
+    # Test /about
+    get about_path
+    assert_select "title", full_title("About")
+
+    # Test /contact
+    get contact_path
+    assert_select "title", full_title("Contact")
+
+    # Test /help
+    get help_path
+    assert_select "title", full_title("Help")
+
+    ## Users Pages
+    # Test /users/<id>
+    get user_path(@user)
+    assert_select "title", full_title("#{@user.name}")
+
+    # Test /users/<id>/edit
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
+
+    # Test /users
+    get users_path
+    assert_select "title", full_title("All Users")
   end
 end
