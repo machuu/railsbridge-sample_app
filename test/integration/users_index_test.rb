8c65c55cf7a75d8e75cc5e5bb0b6b1e580dcdda5
Matt Hoover <matt@hoover.ml>
2017-10-09 11:06:23 +0000

    10.4.3 Listing 10.62: Test admin vs non-admin behavior in `users_index_test`
    - both users see pagination
    - admin sees `delete` links for each user
    - non-admin sees no `delete` links

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,16 +3,33 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
+    @admin = users(:test_admin_1)
     @user = users(:test_user_1)
   end
 
-  test "index includes pagination" do
+  test "index includes pagination, but not delete links, when logged in as non-admin" do
     log_in_as(@user)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index includes pagination and delete links when logged in as admin" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination', count: 2
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
   end
 end
