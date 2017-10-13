36aa6ba210ab76b49647ba3189ab9411f95ab89f
Matt Hoover <matt@hoover.ml>
2017-10-13 10:15:57 +0000

    11.3.3 Listing 11.35-7: Refactor Activation steps into User model
    Tests still pass!

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Activate user
+  def activate
+    update_attribute(:activated,      true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Send Activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # Check if given token matches the digest
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
