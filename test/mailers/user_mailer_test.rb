0b9b6ace975e48b2164f1cb9665d6e73fb77b16f
Matt Hoover <matt@hoover.ml>
2017-10-13 09:05:33 +0000

    Skip Test for Password Reset until Chapter 12

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -14,6 +14,7 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
+    skip "Wait until Chapter 12 to do Password Reset"
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
