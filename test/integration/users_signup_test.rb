9cc85dbfc90eefc4ac72431c3ca329355f0cddd6
Matt Hoover <matt@hoover.ml>
2017-10-13 09:56:36 +0000

    11.3.3 Listing 11.33: Update Signup test to check Activation activity

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup - all fields invalid" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -35,6 +40,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foobar",
                                          password_confirmation: "foobar" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    # Fail to log in before Activation
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    # Fail Activation with Incorrect Token, Correct Email
+    get edit_account_activation_path("incorrect token", email: user.email)
+    assert_not is_logged_in?
+
+    # Fail Activation with Vaild Token, Incorrect Email
+    get edit_account_activation_path(user.activation_token, email: 'incorrect')
+    assert_not is_logged_in?
+
+    # Succeed Activation with Vaild Token, Correct Email
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
+    # Verify Log In completed successfully
     follow_redirect!
     assert_template 'users/show'
     assert flash.present?
