2340bc7c48b7b4dd65b451d8d6aaf16730c97204
Matt Hoover <matt@hoover.ml>
2017-10-15 11:27:23 +0000

    13.2.2 Listing 13.25: Add microposts to *seed* data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,12 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
 99.times do |n|
   name = Faker::Name.name
   email = "user-#{n+1}@example.com"
