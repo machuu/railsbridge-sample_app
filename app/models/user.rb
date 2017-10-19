6d76f5578caaee309ee3f533a326009d09084f0b
Matt Hoover <matt@hoover.ml>
2017-10-19 11:29:11 +0000

    14.1.4 Listing 14.10: Add `follow` methods to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,6 +81,20 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  ## Follow methods
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     def downcase_email
